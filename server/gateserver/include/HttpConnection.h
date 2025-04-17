9d0391a1b1b1cd0b09bec65b2557870fcfe49073
xu orz <1871762140@qq.com>
2025-04-17 16:24:05 +0800

    添加get方法参数解析

--- server/gateserver/include/HttpConnection.h
+++ server/gateserver/include/HttpConnection.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "const.h"
+#include <unordered_map>
 class HttpConnection: public std::enable_shared_from_this<HttpConnection>
 {
     friend class LogicSystem;
 public:
     HttpConnection(tcp::socket socket);
     void Start();
+    void PreParseGetParam();
+
 private:
     void CheckDeadline();
     void WriteResponse();
@@ -21,4 +24,7 @@ private:
     // The timer for putting a deadline on connection processing.
     net::steady_timer deadline_{
         _socket.get_executor(), std::chrono::seconds(60) };
+
+    std::string _get_url;
+    std::unordered_map<std::string, std::string> _get_params;
 };
