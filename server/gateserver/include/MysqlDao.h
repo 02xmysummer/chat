825ae59bbf3d7120fcb5c09b8b1f02cd75031ee1
xu orz <1871762140@qq.com>
2025-04-18 16:14:57 +0800

    添加mysql管理模块

---
+++ server/gateserver/include/MysqlDao.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "const.h"
+#include "MySqlPool.h"
+class MysqlDao
+{
+public:
+    MysqlDao();
+    ~MysqlDao();
+    int RegUser(const std::string& name, const std::string& email, const std::string& pwd);
+private:
+    std::unique_ptr<MySqlPool> pool_;
+};
